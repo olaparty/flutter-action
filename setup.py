bcfc5e64e240273a42044a2960a69144249ddd17
chen.zhilei <chen.zhilei@olaola.chat>
2023-10-04 18:56:58 +0800

    test

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ def action():
     arch = arch if arch else 'x64'
     print(f"!!!!111 cache path {cache_path}")
     if not cache_path or cache_path == '':
-        print(f"!!!!222 cache path {cache_path}")
+        print(f"!!!!222 cache path {cache_path} {os.environ.get('USE_CACHE')}")
         cache_path = f"{os.environ.get('RUNNER_TEMP', default='')}/flutter/:channel:-:version:-:arch:"
         if os.environ.get('USE_CACHE') == 'false':
             cache_path = f"{os.environ.get('HOME')}/_flutter/:channel:-:version:-:arch:"
